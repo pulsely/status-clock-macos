1f303c6b18d6cb6afe6aad05efe70a9ed60d8e6e
kenguish <kenguishnospam@gmail.com>
2022-11-12 23:56:15 +0000

    Initial Commit

---
+++ StatusClock/StatusClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatusClockApp.swift
+//  StatusClock
+//
+//  Created by Kenneth Lo on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct StatusClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
